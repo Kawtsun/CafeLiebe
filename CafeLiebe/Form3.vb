eb1f2a13e9ccb4afaabbf9c5fcd5030d68b9253b
Andrews Leeo Matienzo <matienzoandrews@gmail.com>
2023-11-22 17:29:54 +0800

    add minus button

--- CafeLiebe/Form3.vb
+++ CafeLiebe/Form3.vb
@@ -1,3 +1,17 @@
 ﻿Public Class Form3
+    Dim qty1, qty2, qty3, qty4, qty5, qty6, qty7, qty8 As Integer
 
+    Private Sub btnCQAdd01_Click(sender As Object, e As EventArgs) Handles btnCQAdd01.Click
+        If qty1 < 10 Then
+            qty1 = qty1 + 1
+            lblCQuantity01.Text = qty1
+        End If
+    End Sub
+
+    Private Sub btnCQMinus01_Click(sender As Object, e As EventArgs) Handles btnCQMinus01.Click
+        If qty1 > 0 Then
+            qty1 = qty1 - 1
+            lblCQuantity01.Text = qty1
+        End If
+    End Sub
 End Class
